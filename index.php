9367c2a592bef439409f1da2026afc5afc0f9c00
immracee <42975350+immracee@users.noreply.github.com>
2018-11-26 14:51:41 -0500

    In-progress CSS
    Created food container button, sidebar, styled elements, minor layout adjustments.

--- index.php
+++ index.php
@@ -2,33 +2,58 @@
 include_once 'shared/header.php';
 require_once 'database.php';
 ?>
+<link rel="stylesheet" href="<?= ROOT_URI ?>css/main.css">
+<body style="float: right; margin-right: 3%;" class="bg-grey">
 <main class="text-dark">
-    <h1>My Food</h1>
-    <form action="" method="post">
-        <label for="sort">Sort by</label>
-        <select id="sort" name="sort">
-            <option value="date">Expiry Date</option>
-            <option value="category">Category</option>
-        </select>
-        <button>Go</button>
-    </form>
+  <form  style="float: right;" action="" method="post">
+      <label for="sort text-darkgreen">Sort by</label>
+      <select id="sort" name="sort" class="pulldownbox">
+          <option value="date">Expiry Date</option>
+          <option value="category">Category</option>
+      </select>
+      <button class="btn-green">Go</button>
+  </form>
+  <br>
+    <h1 class="text-green">My Food</h1>
 
-    <div>
-        <h2>This week</h2>
-        <div>
-            <img src="" alt="food category">
-            <h3>Bananas</h3>
-            <p>1 day(s) left</p>
-            <button class="btn-grey">Action</button>
-            <div>
-                <p>Eat</p>
-                <p>Toss</p>
-            </div>
-        </div>
+      <hr />
+
+    <div style="display: inline-block;">
+        <h2 class="text-green">This week</h2>
     </div>
-    <hr />
+
+
+        <div class="foodcontainer">
+            <hr class="redline">
+              <div class="leftfoodcontainer">
+            <img src="" alt="food category">
+              </div>
+              <div ="rightfoodcontainer">
+                <h3 class="text-dark" style="margin: 0px;">Bananas</h3>
+
+                  <hr style="display: inline-block; margin: -3px;" class="containerhr">
+
+                  <p class="text-dark" style="display: inline-block; margin: 0px;">1 day(s) left</p>
+
+                  <br>
+                  <br>
+                  <button class="btn-action" style="display: inline-block;">Action</button>
+              </div>
+              <br>
+                  <div style="display: inline-block;" class="foodcontainerbuttons">
+                    <p style="display: inline;">Eat</p>
+                    <p style="display: inline;">Toss</p>
+                    <p style="display: inline;">Delete</p>
+                  </div>
+
+        </div>
+
+
 </main>
+</body>
+<div style="float: left;">
 <?php
 include_once 'shared/sidebar.php';
 include_once 'shared/footer.php';
 ?>
+</div>
