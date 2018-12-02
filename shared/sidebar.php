0d7282a725da858e0eebbacfe2e0f163074dc3e2
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-12-01 20:32:30 -0500

    added sidebar css

--- shared/sidebar.php
+++ shared/sidebar.php
@@ -1,22 +1,19 @@
 <aside>
     <h2 class="hidden">Sidebar</h2>
-    <img src="" alt="food saviour logo" class="centerall" />
-    <form action="" method="post" class="centerform">
+    <img src="<?= ROOT_URI ?>img/logo.jpg" id="logo" alt="food saviour logo" />
+    <form action="" id="add-form" method="post">
         <label for="add" class="hidden">Add new item: </label>
         <input type="text" id="add" name="add" placeholder="Quick Entry" class="inputbox"/>
-        <button><i class="fas fa-plus"></i><span class="hidden">Add</span></button>
+        <button class="btn-grey"><i class="fas fa-plus"></i><span class="hidden">Add</span></button>
     </form>
     <nav>
         <h3 class="hidden">Navigation</h3>
-
-
         <ul>
           <li class="block"><a href="<?= ROOT_URI ?>index.php">My Food</a></li>
           <li class="block"><a href="<?= ROOT_URI ?>food-saved.php">Food Saved</a></li>
           <li class="block"><a href="<?= ROOT_URI ?>food-lost.php">Food Lost</a></li>
           <li class="block"><a href="<?= ROOT_URI ?>grocery-list.php">Grocery List</a></li>
         </ul>
-
     </nav>
     <section id="footer">
         <h2 class="hidden">Terms and Condition</h2>
