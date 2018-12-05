286d88102fafd47cd077d267280e8762a952f510
NgocTo <baongoc1308@gmail.com>
2018-12-05 11:53:50 -0500

    changed sql order by asc

--- process-edit-eat-toss.php
+++ process-edit-eat-toss.php
@@ -1,10 +1,10 @@
 <?php
 
-header("Location: index.php");
 require_once('database.php');
 $userId = $_POST ['userId'];
 $foodId = $_POST['foodId'];
 $foodState = $_POST['foodState'];
 $items= $pdo->prepare("UPDATE `userfood` SET `foodState` = '$foodState' WHERE `foodId` = '$foodId' AND `userId` = '$userId'");
 $items->execute();
+header("Location: index.php");
 ?>
--- process-read-eat.php
+++ process-read-eat.php
@@ -3,7 +3,7 @@ require_once('database.php');
 $items= $pdo->prepare("SELECT * FROM `userfood` JOIN `food` ON `userfood`.`foodId` = `food`.`id`
                                                 JOIN `categories` ON `food`.`categoryId` = `categories`.`id`
                                                 WHERE `userfood`.`foodState` = 'eat'
-                                                ORDER BY `food`.`foodName` DESC");
+                                                ORDER BY `food`.`foodName`");
 $items->execute();
 $results = $items->fetchAll();
 ?>
