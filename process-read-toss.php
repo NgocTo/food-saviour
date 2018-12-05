9dc9f80609c679caaa228f97cacc45ac4d735c19
NgocTo <baongoc1308@gmail.com>
2018-12-05 12:56:52 -0500

    fixed read according to user id

--- process-read-toss.php
+++ process-read-toss.php
@@ -2,7 +2,7 @@
 require_once('database.php');
 $items= $pdo->prepare("SELECT * FROM `userfood` JOIN `food` ON `userfood`.`foodId` = `food`.`id`
                                                 JOIN `categories` ON `food`.`categoryId` = `categories`.`id`
-                                                WHERE `userfood`.`foodState` = 'toss'
+                                                WHERE (`userfood`.`foodState` = 'toss') AND (`userfood`.`userId` = '$userId')
                                                 ORDER BY `food`.`foodName`");
 $items->execute();
 $results = $items->fetchAll();
--- process-read-userfood.php
+++ process-read-userfood.php
@@ -2,7 +2,7 @@
 require_once('database.php');
 $items= $pdo->prepare("SELECT * FROM `userfood` JOIN `food` ON `userfood`.`foodId` = `food`.`id`
                                                 JOIN `categories` ON `food`.`categoryId` = `categories`.`id`
-                                                WHERE  `userfood`.`foodState` IS NULL
+                                                WHERE (`userfood`.`foodState` IS NULL) AND (`userfood`.`userId` = '$userId')
                                                 ORDER BY `food`.`duration` ASC");
 $items->execute();
 $results = $items->fetchAll();
