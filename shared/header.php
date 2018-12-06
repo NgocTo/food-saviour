dbd528c5026e252fd944d2cc7bc6efb7c4a87529
KateLauren93 <kate-carson@outlook.com>
2018-12-06 17:53:46 -0500

    Linked up settings

--- shared/header.php
+++ shared/header.php
@@ -5,15 +5,22 @@
         <button class="btn-grey"><span class="hidden">Search</span><i class="fas fa-search"></i></button>
     </form>
     <ul id="header-nav">
-        <li><a href="<?= ROOT_URI ?>">
-            <i class="fas fa-home" title="Home"></i>
-            <span class="hidden">Home</span>
-        </a></li>
-        <li id="setting-btn"><i class="fas fa-cog" title="Settings"></i><span class="hidden">Settings</span>
-            <ul id="dropdownlist" class="hide">
-                <li><a href="<?= ROOT_URI ?>process-logout.php">Logout</a></li>
-                <li><a href="<?= ROOT_URI ?>settings.php">Settings</a></li>
-            </ul>
+        <li>
+            <a href="<?= ROOT_URI ?>">
+                <i class="fas fa-home" title="Home"></i>
+                <span class="hidden">Home</span>
+            </a>
+        </li>
+        <li id="setting-btn">
+            <a href="<?= ROOT_URI ?>settings-general.php">
+                <i class="fas fa-cog" title="Settings"></i>
+                <span class="hidden">Settings</span>
+            </a>
+            <!-- <ul id="dropdownlist" class="hide">
+                <li><a href="process-logout.php">Logout</a></li>
+                <li><a href="settings-general.php">Settings</a></li>
+                remember to put ROOT_URI back in front of href
+            </ul> -->
         </li>
     </ul>
 </header>
