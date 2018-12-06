d01f86872212cfc50c0662b0a458f98e9acb47f8
KateLauren93 <kate-carson@outlook.com>
2018-12-06 14:40:55 -0500

    Settings about us finished

--- settings.php
+++ settings.php
@@ -12,7 +12,7 @@ include_once 'shared/header.php'
 ?>
 
 <main class="text-dark">
-    <h1 class="text-green">About Us</h1>
+    <h1 class="text-green settings-padding">About Us</h1>
     <div class="about-container">
         <section class="about">
             <h2 class="text-green">Breakfast Club</h2>
@@ -21,29 +21,31 @@ include_once 'shared/header.php'
         </section>
         <section class="contact">
             <h2 class="text-green">Contact Us</h2>
-            <p>1430 Trafalgar Road, Oakville, Ontario L6H 2L1</p>
+            <p>1430 Trafalgar Road</p>
+            <p>Oakville, Ontario</p>
+            <p>Canada</p>
             <a href="mailto:info@breakfastclub.com">info@breakfastclub.com</a>
         </section>
     </div>
-    <h1 class="text-green">Members</h1>
+    <h1 class="text-green settings-padding">Members</h1>
     <div class="member-container">
         <section class="member">
-            <img src="#" alt="Katie Carson Photo">
+            <img src="img/katie-carson.jpg" alt="Katie Carson Photo" class="img-responsive">
             <h2 class="text-green">Katie Carson</h2>
             <p>I am a marketer with a BA in English, Psychology, and Digital Art Communication. My background is in digital marketing, web content development, and graphic design. I am passionate about design and layout, and creating balanced but also engaging and functional web solutions. A website should stand out, but for the right reasons.</p>
         </section>
         <section class="member">
-            <img src="#" alt="Wren Chen Photo">
+            <img src="img/wren-chen.jpg" alt="Wren Chen Photo" class="img-responsive">
             <h2 class="text-green">Wren Chen</h2>
             <p>I am a UX designer with a BA in English and Art and Art History with experience in photography, graphic design, and content creation. I have a strong passion for environmental issues including waste reduction, environmental protection, and nature conservation.</p>
         </section>
         <section class="member">
-            <img src="#" alt="Elena Race Photo">
+            <img src="img/elena-race.jpg" alt="Elena Race Photo" class="img-responsive">
             <h2 class="text-green">Elena Race</h2>
             <p>I am very keen about technology and the arts, and the intersection of technology with society. I have a BFA in New Media, a multidisciplinary degree with a digital focus. I enjoy following technology news, including critically examining how technology impacts society.</p>
         </section>
         <section class="member">
-            <img src="#" alt="Ngoc To Photo">
+            <img src="img/ngoc-to.jpg" alt="Ngoc To Photo" class="img-responsive">
             <h2 class="text-green">Ngoc To</h2>
             <p>I am a Web Developer who has a background in art and a strong passion for design. I love to create friendly designs with unique layouts that are practical. I am also eager to explore new solutions to existing problems. As front-end, I ensure that the websites I make provide the best experience possible to the users.</p>
         </section>
