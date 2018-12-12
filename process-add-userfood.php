ac1613d042497d2b86f36e89fb11308109977488
KateLauren93 <kate-carson@outlook.com>
2018-12-12 16:36:36 -0500

    Changed date

--- process-add-userfood.php
+++ process-add-userfood.php
@@ -3,6 +3,7 @@ session_start();
 require_once('database.php');
 $userId = $_SESSION['id'];
 $foodName = $_POST['foodName'];
+$dateBought = $_POST['dateBought'];
 $customDurationPost = $_POST['customDuration'];
 $customAmountPost = $_POST['customAmount'];
 $customDuration = ($customDurationPost != "")? "$customDurationPost" : "null";
@@ -10,15 +11,17 @@ $customAmount = ($customAmountPost != "")? "$customAmountPost" : "null";
 $stmt = $pdo->prepare("SELECT * FROM `food` WHERE LOWER(`foodName`) = LOWER('$foodName')");
 $stmt->execute();
 $row = $stmt->fetch();
+var_dump($dateBought);
 if($row) {
     $foodId = $row['id'];
-    $stmt = $pdo->prepare("INSERT INTO `userfood`(`userId`, `foodId`, `customDuration`, `customAmount`)
-    VALUES ('$userId', '$foodId', $customDuration, $customAmount)");
+    $stmt = $pdo->prepare("INSERT INTO `userfood`(`userId`, `foodId`, `date`, `customDuration`, `customAmount`)
+    VALUES ('$userId', '$foodId', '$dateBought', $customDuration, $customAmount)");
 } else {
     $foodId = 14; // custom food id
-    $stmt = $pdo->prepare("INSERT INTO `userfood`(`userId`, `foodId`, `customFoodName`, `customDuration`, `customAmount`)
-    VALUES ('$userId', '$foodId', '$foodName', $customDuration, $customAmount)");
+    $stmt = $pdo->prepare("INSERT INTO `userfood`(`userId`, `foodId`, `date`, `customFoodName`, `customDuration`, `customAmount`)
+    VALUES ('$userId', '$foodId', '$dateBought', '$foodName', $customDuration, $customAmount)");
 }
-$stmt->execute();
-header("Location: index.php");
+$count = $stmt->execute();
+echo $count;
+// header("Location: index.php");
 ?>
